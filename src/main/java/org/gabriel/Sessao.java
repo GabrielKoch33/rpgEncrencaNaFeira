dc7a103c8cbd190873675f7daa85463f4f9b5060
Gabriel Koch da Silva <gabrielkochsilva@gmail.com>
2026-09-17 21:43:06 -0300

    comentario

--- src/main/java/org/gabriel/Sessao.java
+++ src/main/java/org/gabriel/Sessao.java
@@ -2,7 +2,13 @@ package org.gabriel;
 
 import org.gabriel.classesMecanicas.Item;
 import org.gabriel.classesMecanicas.Loja;
-
+/**
+ * 1 -> Atributos static <br><br>
+ * 2 -> Blocos de inicialização static<br><br>
+ * 3 -> Atributos não-static (de instância)<br><br>
+ * 4 -> Blocos de inicialização não-static (de instância)<br><br>
+ * 5 -> Construtor
+ * **/
 public class Sessao {
     public static void main(String[] args) {
         /* na MAIN
